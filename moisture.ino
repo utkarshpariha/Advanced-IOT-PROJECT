d7292ec08215e1623fdd07a5ef1bc898e8a65d58
Utkarsh Singh Parihar <103374960+utkarshpariha@users.noreply.github.com>
2024-08-28 23:28:21 +0530

    Update moisture.ino

--- moisture.ino
+++ moisture.ino
@@ -12,10 +12,15 @@ void setup(void){
   Serial.begin(115200);
   servo1.attach(A0);     
 }
-
+// he attach() function also initializes the timer for PWM control.
 void loop(void){
   sensor_analog = analogRead(sensor_pin);
-  _moisture = int(100.0-(float(sensor_analog) / 4095.00) * 100);
+  // - `float(sensor_analog)`: Converts the integer `sensor_analog` value to a floating-point number for precise division.
+  // - `sensor_analog / 4095.00`: Normalizes the sensor value to a range between 0.0 and 1.0. (Assuming a 12-bit ADC, which has a maximum value of 4095.)
+  // - `(float(sensor_analog) / 4095.00) * 100`: Converts the normalized value to a percentage.
+  // - `100.0 - ...`: Subtracts this value from 100 to invert it, assuming higher analog values correspond to drier conditions (less moisture).
+  // - `int(...)`: Converts the floating-point percentage back to an integer, storing it in `_moisture`.
+_moisture = int(100.0-(float(sensor_analog) / 4095.00) * 100);
   Serial.print("Moisture = ");
   Serial.print(_moisture);  
   Serial.print("%");
